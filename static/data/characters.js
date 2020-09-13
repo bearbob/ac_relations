92f895d752c337b47a52afdd37b02cff3322a5fe
Björn Schmidt <bearbob@users.noreply.github.com>
2020-09-13 12:52:55 +0200

    Fixed episode handling

--- static/data/characters.js
+++ static/data/characters.js
@@ -1,72 +1,72 @@
 const raw_data = {
   "nodes": [
     {
-      "id": "Tadamir",
+      "uid": "Tadamir",
       "episode": 0,
       "group": 1,
       "isPlayer": true,
       "description": "Tadamir ist ein menschlicher Barde, der in einer behüteten Familie mit vielen Geschwistern aufwuchs."
     },
     {
-      "id": "Jin",
+      "uid": "Jin",
       "episode": 0,
       "group": 1,
       "isPlayer": true,
       "description": "Jin ist ein Changeling, der im Auftrag der Göttin Titania ruchlose Hexenmeister tötet, um das Gleichgewicht der Mächte wieder herzustellen. Er ist oft in der Gestalt einer alten Bekannten zu sehen."
     },
     {
-      "id": "Parmigianino",
+      "uid": "Parmigianino",
       "episode": 0,
       "group": 1,
       "isPlayer": true,
       "description": "Meist 'Nino' genannt, ist ein drahtiger Tortle, der mit seinen Brüdern nach den Lehren des Daikoku-ten erzogen wurde."
     },
     {
-      "id": "Orveyl",
+      "uid": "Orveyl",
       "episode": 0,
       "group": 1,
       "isPlayer": true,
       "description": "Ein abenteuerlustiger Zentaure, der sich nach einigen Verlusten am Kult der Maschinenmutter rächen will."
     },
     {
-      "id": "Bhavin",
+      "uid": "Bhavin",
       "episode": 0,
       "group": 1,
       "isPlayer": true,
       "description": "Bhavin ist ein Kalashtar, der auf einer Hochebene aufwuchs und die Magie der Wiederherstellung erlernte. Er wohnt mit seiner Frau und den Kindern auf einem Bauernhof in einem Wald. Als sein Wassergeist ihm den Auftrag gab Jin zu finden und zu folgen, begann für ihn das Abenteuer. Er ist gerne in der freien Natur, raucht Pfeife, trinkt gerne Fruchtsäfte und spielt gekonnt die Panflöte."
     },
     {
-      "id": "Koalak",
+      "uid": "Koalak",
       "group": 10,
       "episode": 0,
       "description": "Zentaurischer Schmied, der sich auch gut mit maschinellen Prothesen auskennt."
     },
     {
-      "id": "Yamato Hoshi",
+      "uid": "Yamato Hoshi",
       "group": 6,
       "episode": 0,
       "description": "Lehrmeister des Daikoku-ten-Klosters"
     },
     {
-      "id": "Masolino",
+      "uid": "Masolino",
       "group": 6,
       "episode": 0,
       "description": "Ein kräftiger Tortle, der mit seinen Brüdern nach den Lehren des Daikoku-ten erzogen wurde."
     },
     {
-      "id": "Bronzino",
+      "uid": "Bronzino",
       "group": 6,
       "episode": 0,
       "description": "Ein pfiffiger Tortle, der mit seinen Brüdern nach den Lehren des Daikoku-ten erzogen wurde."
     },
     {
-      "id": "Filippino",
+      "uid": "Filippino",
       "group": 6,
       "episode": 0,
       "description": "Ein vorlauter Tortle, der mit seinen Brüdern nach den Lehren des Daikoku-ten erzogen wurde."
     },
     {
-      "id": "Alba",
+      "uid": "Alba",
       "group": 2,
       "episode": 14,
       "description": [
@@ -81,7 +81,7 @@ const raw_data = {
       ]
     },
     {
-      "id": "Osmonias",
+      "uid": "Osmonias",
       "names": [
         {
           "text": "???",
@@ -93,13 +93,13 @@ const raw_data = {
       "description": "???"
     },
     {
-      "id": "Falana",
+      "uid": "Falana",
       "group": 2,
       "episode": 0,
       "description": "Ein elfische Spionin und vermeintliche Diebin"
     },
     {
-      "id": "Leoric",
+      "uid": "Leoric",
       "names": [
         {
           "text": "Der Barde",
@@ -111,32 +111,32 @@ const raw_data = {
       "description": "Ein berühmter Barde und Wanderer, der oft ohne Ziel und Karte unterwegs war"
     },
     {
-      "id": "Kult der Maschinenmutter",
+      "uid": "Kult der Maschinenmutter",
       "group": 3,
       "episode": 1,
       "description": "Die Anhänger des Kults glauben an die Überlegenheit von Maschinen gegenüber lebenden Wesen. Sie glauben an das Recht des Stärkeren und überfallen ohne Gnade Dörfer und sogar Städte, wenn sie glauben dort interessante Maschinenteile zu erbeuten.",
       "isFaction": true
     },
     {
-      "id": "Smasher",
+      "uid": "Smasher",
       "group": 3,
       "episode": 6,
       "description": "General im Kult der Maschinenmutter. Er ist ein muskulöser, dunkelhäutiger Mann, dessen Arme ab den Schultern durch mechanische Arme ersetzt wurden. Mit diesen Armen ist er sogar in der Lage Steinwände zu durchschlagen, wie er bereits mehrfach unter Beweis stellte."
     },
     {
-      "id": "Laserbeam",
+      "uid": "Laserbeam",
       "group": 3,
       "episode": 3,
       "description": "General im Kult der Maschinenmutter. Er ist ein großer, breitschultriger Mann mit langem blonden Haar und stechenden, grünen Augen. Sein Blick verrät den Wahnsinn der in seinem Kopf vorgeht, die Nase sieht aus als wäre sie schon mehrfach gebrochen worden. Sein linker Arm ist von einem glänzenden Metall ummantelt, dessen Ende wie ein kleines Kanonenrohr aussieht."
     },
     {
-      "id": "Loxxkatron",
+      "uid": "Loxxkatron",
       "group": 4,
       "episode": 7,
       "description": "Elfling, der angeblich vom Kult der Maschinenmutter gefangen genommen wurde und mit den Helden flüchtete. Die grüne Irokesenfrisur und sein Holzbein sind sehr auffällig."
     },
     {
-      "id": "Grace",
+      "uid": "Grace",
       "names": [
         {
           "text": "Die alte Frau",
@@ -147,7 +147,7 @@ const raw_data = {
           "episode": 20
         }
       ],
-      "group": 2,
+      "group": "Tinka",
       "episode": 12,
       "description": [
         {
@@ -165,88 +165,94 @@ const raw_data = {
       ]
     },
     {
-      "id": "Die Spielzeugsoldaten",
+      "uid": "Die Spielzeugsoldaten",
       "group": 4,
       "isFaction": true,
       "episode": 11,
       "description": "Die größte kriminelle Vereinigung in der Stadt des Riesen"
     },
     {
-      "id": "Gorev",
+      "uid": "Gorev",
       "group": 4,
       "episode": 11,
       "description": "Auch bekannt als \"Der Nussknacker\", mächtiger Untergrundboss in der Stadt des Riesen"
     },
     {
-      "id": "Amanda",
+      "uid": "Amanda",
       "group": 4,
       "episode": 11,
       "description": "Handlangerin von Gorev"
     },
     {
-      "id": "Sandra",
+      "uid": "Sandra",
       "group": 4,
       "episode": 18,
       "description": "Gorevs rechte Hand. Eine junge Frau mit kurzen blonden Haaren, die fast immer ein schelmisches Grinsen aufgelegt hat."
     },
     {
-      "id": "Hauptmann David",
+      "uid": "Hauptmann David",
       "group": 5,
       "episode": 10,
       "description": "David war Hauptmann der Stadtwache in der Stadt des Riesen und für das Stadttor verantwortlich."
     },
     {
-      "id": "Liliana",
+      "uid": "Liliana",
       "group": 5,
       "episode": 12,
       "description": "Liliana von der Hagelweide, Stadträtin in der Stadt des Riesen."
     },
     {
-      "id": "Ton",
+      "uid": "Ton",
       "group": 4,
       "episode": 11,
       "description": "Ein männlicher Elf, der in der Stadt des Riesen für Gorev arbeitet. Er ist recht schweigsam und beobachtet gern aus der Ferne."
     },
     {
-      "id": "Titania",
-      "group": 100,
+      "uid": "Titania",
+      "group": "Gods",
       "episode": 1,
       "description": "Göttin der Fey und Herrscherin des Sommerreichs"
     },
     {
-      "id": "Lajak",
-      "group": 100,
+      "uid": "Titanias Schatten",
+      "group": "Gods",
+      "episode": 17,
+      "description": "Ein Schatten, der sich von der Göttin der Fey löste und nun eine eigene Agenda verfolgt."
+    },
+    {
+      "uid": "Lajak",
+      "group": "Gods",
       "episode": 1,
       "description": "Gott des Kampfes und des Windes"
     },
     {
-      "id": "Dryasis",
-      "group": 100,
+      "uid": "Dryasis",
+      "group": "Gods",
       "episode": 12,
       "description": "Göttin des Tages, der Sonne und des Friedens. Sie ist eine der weit verbreitetsten Gottheiten in der zerissenen Welt. Ihr Symbol ist der Sonnenaufgang (ein waagerechter Strich mit einem Halbkreis auf der oberen Seite, der eine Sonne symbolisiert). "
     },
     {
-      "id": "Clint",
+      "uid": "Clint",
       "group": 11,
       "episode": 9,
       "description": "Ein verbitterter alter Mann, der allein in einem vom Kult der Maschinenmutter zerstörten Dorf lebt."
     },
     {
-      "id": "Wrakenberg",
+      "uid": "Wrakenberg",
       "group": 13,
       "isFaction": true,
       "episode": 2,
       "description": "Ein kleines Dorf, das beinahe Opfer des Kults der Maschinenmutter geworden wäre."
     },
     {
-      "id": "Tinka",
+      "uid": "Tinka",
       "group": "Tinka",
       "isFaction": true,
       "episode": 18,
       "description": "Eine große Nation im Süden. Einst ein Königreich, wurde es durch eine blutige Revolution zur Republik und ist heute Ursprung des Kults der Maschinenmutter."
     },
     {
-      "id": "David Friedensbringer",
+      "uid": "David Friedensbringer",
       "group": "Tinka",
       "episode": 21,
       "description": "Ehemaliger Anführer der blutigen Revolution von Tinka."
@@ -257,42 +263,32 @@ const raw_data = {
       "source": "Jin",
       "target": "Falana",
       "episode": 0,
-      "id": "Alte Bekannte",
+      "uid": "Alte Bekannte",
       "description": "Jin ist seit Jahren auf der Suche nach Falana, da sie ihm einen wichtigen Gegenstand gestohlen hat.",
       "value": 3
     },
     {
       "source": "Jin",
       "target": "Alba",
-      "id": "Versuchter Mord",
+      "uid": "Versuchter Mord",
       "description": "Alba ist die Überlebende eines Angriffs, den Jin im Auftrag seiner Göttin ausgeführt hat. Jin überlebte den Kampf nur knapp und wähnte Alba tot.",
       "episode": 16,
       "value": 3
     },
-    /*
     {
-      "source": "Tadamir",
-      "target": "Alba",
-      "episode": 14,
-      "id": "Zufällige Begegnung",
-      "description": "Tadamir ist Alba zufällig auf dem Markt 'Der Nabel' über den Weg gelaufen und hat sie, aufgrund der gleichen Gestalt, mit Jin verwechselt. Es hat einen Moment gedauert, bis er seinen Irrtum bemerkt hat.",
+      "source": "Alba",
+      "target": "Tadamir",
+      "episode": 21,
+      "uid": "Neugier",
+      "description": "Nachdem Tadamir sein Geheimnis enthüllt, ist Alba versessen darauf ihn genauer zu untersuchen.",
       "value": 1
     },
-    {
-      "source": "Parmigianino",
-      "target": "Alba",
-      "id": "Zufällige Begegnung",
-      "description": "Nino hat Alba im Stadtpark der Stadt des Riesen gesehen und schnell verstanden, dass die Elfe vermutlich die Person ist, nach der Jin sucht.",
-      "episode": 12,
-      "value": 1
-    },
-    */
     {
       "source": "Parmigianino",
       "target": "Yamato Hoshi",
       "episode": 0,
       "value": 3,
-      "id": "Ziehvater",
+      "uid": "Ziehvater",
       "description": "Meister Hoshi zog Nino und seine Brüder auf"
     },
     {
@@ -300,7 +296,7 @@ const raw_data = {
       "target": "Masolino",
       "episode": 0,
       "value": 4,
-      "id": "Brüder",
+      "uid": "Brüder",
       "description": "Masolino und Parmigianino sind Brüder, die als Eier vor dem Kloster von Meister Hoshi aufgefunden wurden."
     },
     {
@@ -308,7 +304,7 @@ const raw_data = {
       "target": "Yamato Hoshi",
       "episode": 0,
       "value": 3,
-      "id": "Ziehvater",
+      "uid": "Ziehvater",
       "description": "Meister Hoshi zog Masolino und seine Brüder auf"
     },
     {
@@ -316,7 +312,7 @@ const raw_data = {
       "target": "Bronzino",
       "episode": 0,
       "value": 4,
-      "id": "Brüder",
+      "uid": "Brüder",
       "description": "Bronzino und Parmigianino sind Brüder, die als Eier vor dem Kloster von Meister Hoshi aufgefunden wurden."
     },
     {
@@ -324,7 +320,7 @@ const raw_data = {
       "target": "Yamato Hoshi",
       "episode": 0,
       "value": 3,
-      "id": "Ziehvater",
+      "uid": "Ziehvater",
       "description": "Meister Hoshi zog Bronzino und seine Brüder auf"
     },
     {
@@ -332,7 +328,7 @@ const raw_data = {
       "target": "Filippino",
       "episode": 0,
       "value": 4,
-      "id": "Brüder",
+      "uid": "Brüder",
       "description": "Filippino und Parmigianino sind Brüder, die als Eier vor dem Kloster von Meister Hoshi aufgefunden wurden."
     },
     {
@@ -340,23 +336,13 @@ const raw_data = {
       "target": "Yamato Hoshi",
       "episode": 0,
       "value": 3,
-      "id": "Ziehvater",
+      "uid": "Ziehvater",
       "description": "Meister Hoshi zog Filippino und seine Brüder auf"
     },
-    /*
-    {
-      "source": "Bhavin",
-      "target": "Alba",
-      "id": "Zufällige Begegnung",
-      "description": "Bhavin hat Alba im Stadtpark der Stadt des Riesen gesehen und schnell verstanden, dass die Elfe vermutlich die Person ist, nach der Jin sucht.",
-      "episode": 12,
-      "value": 1
-    },
-    */
     {
       "source": "Grace",
       "target": "Alba",
-      "id": "Neugier",
+      "uid": "Neugier",
       "description": "Alba hat Grace in der Stadt des Riesen aufgesucht. Sie ließ sich von ihr bei der Übersetzung eines alten Buches helfen.",
       "episode": 12,
       "value": 1
@@ -366,7 +352,7 @@ const raw_data = {
       "target": "Kult der Maschinenmutter",
       "episode": 0,
       "value": 2,
-      "id": "Rache",
+      "uid": "Rache",
       "description": "Bei einem Zusammentreffen mit den Kultisten verlor Orveyl sein linkes Vorderbein und viele Freunde."
     },
     {
@@ -374,7 +360,7 @@ const raw_data = {
       "target": "Smasher",
       "episode": 0,
       "value": 2,
-      "id": "Rache",
+      "uid": "Rache",
       "description": "Smasher ist für die Zerstörung von Ninos Kloster verantwortlich. Seit dem Vorfall wird Meister Hoshi vermisst und Nino hat Rache geschworen."
     },
     {
@@ -382,13 +368,13 @@ const raw_data = {
       "target": "Kult der Maschinenmutter",
       "episode": 0,
       "value": 2,
-      "id": "General",
+      "uid": "General",
       "description": "Laserbeam ist einer der Anführer des Kults"
     },
     {
       "source": "Laserbeam",
       "target": "Kult der Maschinenmutter",
-      "id": "General",
+      "uid": "General",
       "description": "Laserbeam ist einer der Anführer des Kults",
       "episode": 0,
       "value": 2
@@ -396,7 +382,7 @@ const raw_data = {
     {
       "source": "Laserbeam",
       "target": "Wrakenberg",
-      "id": "Überfall",
+      "uid": "Überfall",
       "description": "Laserbeam hat versucht das Dorf Wrakenberg zu überfallen",
       "episode": 3,
       "value": 1
@@ -404,7 +390,7 @@ const raw_data = {
     {
       "source": "Tadamir",
       "target": "Wrakenberg",
-      "id": "Bürgermeister",
+      "uid": "Bürgermeister",
       "description": "Durch unlautere politische Tricks ist Tadamir zum reisenden Bürgermeister des Dorfes geworden.",
       "episode": 5,
       "value": 1
@@ -420,7 +406,7 @@ const raw_data = {
       "target": "Jin",
       "episode": 0,
       "value": 3,
-      "id": "Schicksalhafte Begegnung",
+      "uid": "Schicksalhafte Begegnung",
       "description": "Bhavin ist seinem Geist gefolgt und hat dabei den beinahe toten Jin gefunden. Er hat es als Zeichen gedeutet, dass er Jin finden und das Leben retten sollte. Seitdem ist er mit ihm unterwegs, um zu erfahren, was das Schicksal mit ihm vorhat."
     },
     {
@@ -502,6 +488,24 @@ const raw_data = {
       "value": 2
     },
     {
+      "source": "Jin",
+      "target": "Titanias Schatten",
+      "episode": 17,
+      "value": 2
+    },
+    {
+      "source": "Alba",
+      "target": "Titania",
+      "episode": 16,
+      "value": 2
+    },
+    {
+      "source": "Titanias Schatten",
+      "target": "Titania",
+      "episode": 17,
+      "value": 3
+    },
+    {
       "source": "Orveyl",
       "target": "Lajak",
       "episode": 1,
@@ -512,7 +516,7 @@ const raw_data = {
       "target": "Koalak",
       "episode": 1,
       "value": 2,
-      "id": "Alte Freunde",
+      "uid": "Alte Freunde",
       "description": "Koalak half Orveyl nach dem Verlust seines Beins mit einer künstlichen Prothese"
     },
     {
@@ -556,7 +560,7 @@ const raw_data = {
       "target": "Clint",
       "episode": 9,
       "value": 1,
-      "id": "Sterbehilfe",
+      "uid": "Sterbehilfe",
       "description": "Jin hat Clint seine Hilfe angeboten, falls er sterben wollte. Clint hat das Angebot ausgeschlagen und sich kurz darauf erschossen."
     },
     {
@@ -564,7 +568,7 @@ const raw_data = {
       "target": "Osmonias",
       "episode": 16,
       "value": 1,
-      "id": "Spurensuche",
+      "uid": "Spurensuche",
       "description": "Auf der Suche nach altem Wissen ist Alba auf eine Spur zu einem der Alten gestoßen."
     },
     {
@@ -572,7 +576,7 @@ const raw_data = {
       "target": "Gorev",
       "episode": 20,
       "value": 1,
-      "id": "Deal",
+      "uid": "Deal",
       "description": "Um den Riesen zu erwecken, ist Alba einen Deal mit Gorev eingegangen."
     },
     {
@@ -580,7 +584,7 @@ const raw_data = {
       "target": "Gorev",
       "episode": 20,
       "value": 4,
-      "id": "Blutpakt",
+      "uid": "Blutpakt",
       "description": "Tadamir ist einen Blutpakt mit Gorev eingegangen, um gegen den Kult der Maschinenmutter in den Kampf zu ziehen."
     },
     {
@@ -588,7 +592,7 @@ const raw_data = {
       "target": "Tinka",
       "episode": 18,
       "value": 1,
-      "id": "Heimat",
+      "uid": "Heimat",
       "description": "Tadamir ist in Tinka geboren und aufgewachsen."
     },
     {
@@ -596,7 +600,7 @@ const raw_data = {
       "target": "Tinka",
       "episode": 18,
       "value": 1,
-      "id": "Heimat",
+      "uid": "Heimat",
       "description": "Grace war früher Gelehrte am Hof von Tinka."
     },
     {
@@ -604,7 +608,7 @@ const raw_data = {
       "target": "Tinka",
       "episode": 21,
       "value": 2,
-      "id": "Heimat",
+      "uid": "Heimat",
       "description": "David ist in Tinka geborgen und war Teil der Regierung der Republik."
     },
     {
@@ -612,57 +616,8 @@ const raw_data = {
       "target": "Orveyl",
       "episode": 21,
       "value": 1,
-      "id": "Symbol",
+      "uid": "Symbol",
       "description": "David will Orveyl zum Symbol des Widerstands gegen den Kult machen."
     }
   ]
-};
-
-const getFormattedData = function() {
-  let identifier = 1;
-  let mapping = {};
-  let nodes = [];
-  let edges = [];
-
-  raw_data.nodes.forEach(character => {
-    character.label = character.id;
-    character.id = identifier;
-    //TODO Linebreak and determining which description to use
-    let regExLineBreak = /\. /gi;
-    character.title = character.description;
-    if(typeof character.title == 'string') {
-      character.title = character.title.replace(regExLineBreak, '. <br/>');
-    }
-    if (character.isPlayer) {
-      character.borderWidth = 3;
-      character.borderWidthSelected = 4;
-    }
-    if (character.isFaction) {
-      character.shape = 'box';
-    }
-    nodes.push(character);
-
-    mapping[character.label] = identifier;
-    identifier++;
-  });
-
-  raw_data.links.forEach(link => {
-    let edge = {
-      from: mapping[link.source],
-      to: mapping[link.target],
-      label: link.id,
-      title: link.description,
-    };
-    if(link.value === 1) {
-      edge.dashes = true;
-    }
-
-    edges.push(edge);
-  });
-
-  return {
-    nodes: nodes,
-    edges: edges,
-  };
-
 };
--- static/graph/graph.js
+++ static/graph/graph.js
@@ -1,12 +1,83 @@
-//some constants for the graph
-var svg;
+
+const getFormattedData = function(nEpisode) {
+  nEpisode = nEpisode || 999;
+  let identifier = 1;
+  let mapping = {};
+  let nodes = [];
+  let edges = [];
+
+  raw_data.nodes.forEach(character => {
+    if (character.episode <= nEpisode) {
+
+      character.id = identifier;
+      character.label = character.uid;
+      if(character.names) {
+        character.names.sort( (a,b) => b.episode - a.episode);
+        let names = character.names.filter( c => c.episode <= nEpisode);
+        if(names.length > 0) {
+          character.label = names[0].text;
+        }
+      }
+
+      //TODO Linebreak and determining which description to use
+      let regExLineBreak = /\. /gi;
+      character.title = character.description;
+      if(typeof character.description != "string") {
+        character.description.sort( (a,b) => b.episode - a.episode);
+        let descr = character.description.filter( c => c.episode <= nEpisode);
+        if(descr.length > 0) {
+          character.title = descr[0].text;
+        }
+      }
+
+      if(typeof character.title == 'string') {
+        character.title = character.title.replace(regExLineBreak, '. <br/>');
+      }
+      if (character.isPlayer) {
+        character.shape = 'circle';
+      }
+      if (character.isFaction) {
+        character.shape = 'box';
+      }
+      nodes.push(character);
+
+      mapping[character.uid] = identifier;
+      identifier++;
+    }
+  });
+
+  raw_data.links.forEach(link => {
+    if(link.episode <= nEpisode) {
+
+      let edge = {
+        from: mapping[link.source],
+        to: mapping[link.target],
+        label: link.uid,
+        title: link.description,
+      };
+      if(link.value === 1) {
+        edge.dashes = true;
+      }
+
+      edges.push(edge);
+    }
+  });
+
+  return {
+    nodes: nodes,
+    edges: edges,
+  };
+
+};
 
 /**
  * @public
  * Creates the network graph
+ * @param {number} nCurrentEpisode The value of the current episode. Used to hide spoilers
  */
-var buildGraph = function () {
-  let dataInput = getFormattedData();
+const buildGraph = function (nCurrentEpisode) {
+  nCurrentEpisode = nCurrentEpisode || 999;
+  let dataInput = getFormattedData(nCurrentEpisode);
   // create an array with nodes
   var nodes = new vis.DataSet(dataInput.nodes);
 
@@ -28,6 +99,16 @@ var buildGraph = function () {
           highlight: {
             background:'#ffda84',
             border:'#cca751'
+          }
+        },
+      },
+      Gods: {
+        color: {
+          background:'#56CFE1',
+          border:'#5390D9',
+          highlight: {
+            background:'#72EFDD',
+            border:'#5390D9'
           }
         },
       }
